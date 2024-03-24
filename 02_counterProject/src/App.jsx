89711dd06087cf011fbb0c88a6737dc55c869bdf
smit prajapati <smitprajapati002@gmail.com>
2024-03-24 16:19:45 -0600

    Counter project is over

--- 02_counterProject/src/App.jsx
+++ 02_counterProject/src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./App.css"
+
+
 function App() {
-  let counter = 15
+  let [counter, setCounter] = useState(0) 
+
+  // let counter = 15
   const addValue = ()=>{
-    
+    console.log("clicked", counter);
+    counter = counter + 1
+    setCounter(counter  )
   }
   return (
     <>
       <h1>Chai Aur React</h1>   
       <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button>
+      <button onClick={addValue}>Add Value {counter}</button>
       <br />
-      <button>Decreased Value</button>
+      <button>Decreased Value {counter}</button>
+      <p>footer {counter}</p>
     </>
   )
 }
