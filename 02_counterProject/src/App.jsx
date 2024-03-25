fc71c5663482ee0650f29eb97f3e97b6de832c5c
smit prajapati <smitprajapati002@gmail.com>
2024-03-25 16:49:41 -0600

    third project is started

--- 02_counterProject/src/App.jsx
+++ 02_counterProject/src/App.jsx
@@ -3,30 +3,39 @@ import "./App.css";
 
 function App() {
   let [counter, setCounter] = useState(0);
-
+  let [value , setValue] = useState("sonu")
   // let counter = 15
   const addValue = () => {
-    if(counter > 20){
-      counter = "This is an overValue number"
+    if (counter > 20) {
+      counter = "This is an overValue number";
+    } else {
+      setCounter((counter)=> counter + 1)
+      setCounter((counter)=> counter + 1)
+      setCounter((counter)=> counter + 1)
+      setCounter((counter)=> counter + 1)
     }
-    counter = counter + 1;
-    setCounter(counter);
   };
   const decreaseValue = () => {
     if (counter < 0) {
       counter = "You can't decrease negative value";
+    } else {
+      counter = counter - 1;
+      setCounter(counter);
     }
-    counter = counter - 1;
-    setCounter(counter);
   };
+  const changeValue = ()=>{
+    value = ["smit","Prajapati", "Kanubhai", "Keshavlala" ]
+    setValue(value)
+  }
   return (
     <>
       <h1>Chai Aur React</h1>
       <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button>
+      <button onClick={addValue}>Add a Value</button>
       <br />
-      <button onClick={decreaseValue}>Decreased Value</button>
+      <button onClick={decreaseValue}>Decreased a Value</button>
       {/* <p>{message}</p> */}
+      <button onClick={changeValue}>{value}</button>
     </>
   );
 }
