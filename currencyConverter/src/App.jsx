b378401856d91ceeeaa8eae614cec1c2bfed6b31
smit prajapati <smitprajapati002@gmail.com>
2024-04-15 15:26:51 -0600

    passwordGenerator project is over

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,31 +1,11 @@
 
-
-
-// function App() {
-//   function FormatName(user) {
-//     return user.firstName + " " + user.lastName;
-//   }
-//   const muser = {
-//     firstName: "Smit",
-//     lastName: "Prajapati",
-//   };
-//   return (
-//     <>
-//       <h1>Hello, {FormatName(muser)}</h1>
-//     </>
-//   );
-// }
-
-// export default App;
-
-import React, { Component } from 'react'
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+        <h1>Smit Prajapati</h1>      
+    </div>
+  )
 }
+
+export default App
+
